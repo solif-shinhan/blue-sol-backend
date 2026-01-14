647759d4607d5a54d22f753678ebc29fa04fea8c
Jeongdonghun <arrrr180@konkuk.ac.kr>
2026-01-14 13:53:42 +0900

    init: 초기 프로젝트 세팅

---
+++ blue-sol-backend/src/main/java/com/solif/backend/BlueSolBackendApplication.java
@@ -0,0 +1,13 @@
+package com.solif.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueSolBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlueSolBackendApplication.class, args);
+	}
+
+}
---
+++ blue-sol-backend/src/test/java/com/solif/backend/BlueSolBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solif.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueSolBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

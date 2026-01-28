490f2aa69948b0cfb7e3f6165b11e2b1f2389f76
dhun <arrrrr180@gmail.com>
2026-01-28 16:49:04 +0900

    feat: 게시글 도메인 Success/ErrorCode 추가

---
+++ blue-sol-backend/src/main/java/com/solif/backend/domain/post/code/PostErrorCode.java
@@ -0,0 +1,19 @@
+package com.solif.backend.domain.post.code;
+
+import com.solif.backend.global.common.exception.ErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum PostErrorCode implements ErrorCode {
+
+    POST_NOT_FOUND(HttpStatus.NOT_FOUND, "POST_001", "게시글을 찾을 수 없습니다."),
+    UNAUTHORIZED_POST_ACCESS(HttpStatus.FORBIDDEN, "POST_002", "게시글에 대한 권한이 없습니다."),
+    DELETED_POST(HttpStatus.GONE, "POST_003", "삭제된 게시글입니다.");
+
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
+}

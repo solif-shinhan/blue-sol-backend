ba9e3329b729a24ff19206f8470b4117c9729512
장난영 <nanyoung1151@naver.com>
2026-01-29 17:20:55 +0900

    feature: Notification 엔티티 생성

---
+++ blue-sol-backend/src/main/java/com/solif/backend/domain/notification/entity/NotificationType.java
@@ -0,0 +1,8 @@
+package com.solif.backend.domain.notification.entity;
+
+public enum NotificationType {
+    COMMENT,    // 댓글
+    MESSAGE,    // 쪽지
+    CHEER,      // 응원하기
+    HELP        // 도와줄게
+}

25767680ceba9855b6f9d0df232c99f119f7fa4c
장난영 <nanyoung1151@naver.com>
2026-02-03 12:30:17 +0900

    fix: File 엔티티 LAZY 로딩으로 인한 N+1 문제 해결

--- blue-sol-backend/src/main/java/com/solif/backend/domain/file/repository/FileAttachmentRepository.java
+++ blue-sol-backend/src/main/java/com/solif/backend/domain/file/repository/FileAttachmentRepository.java
@@ -2,12 +2,14 @@ package com.solif.backend.domain.file.repository;
 
 import com.solif.backend.domain.file.entity.FileAttachment;
 import com.solif.backend.domain.file.entity.TargetType;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface FileAttachmentRepository extends JpaRepository<FileAttachment, Long> {
 
+    @EntityGraph(attributePaths = {"file"})
     List<FileAttachment> findByTargetTypeAndTargetIdOrderBySortOrder(
             TargetType targetType, Long targetId);
 

6cd93ca2f68603eeee998d865e597c7e71acb959
dhun <arrrrr180@gmail.com>
2026-02-09 09:51:19 +0900

    feat: Message 도메인에 파일 첨부 기능을 추가

--- blue-sol-backend/src/main/java/com/solif/backend/domain/file/entity/AttachmentPurpose.java
+++ blue-sol-backend/src/main/java/com/solif/backend/domain/file/entity/AttachmentPurpose.java
@@ -4,5 +4,6 @@ public enum AttachmentPurpose {
     PROFILE_IMAGE,           // 프로필 이미지
     PINECONE_MEMORY_IMAGE,   // 솔방울 추억 이미지
     POST_ATTACHMENT,         // 게시글 첨부 이미지
-    RECEIPT                  // 영수증 (OCR용)
+    RECEIPT,                 // 영수증 (OCR용)
+    MESSAGE_ATTACHMENT       // 쪽지
 }
--- blue-sol-backend/src/main/java/com/solif/backend/domain/file/entity/FileTargetType.java
+++ blue-sol-backend/src/main/java/com/solif/backend/domain/file/entity/FileTargetType.java
@@ -4,5 +4,6 @@ public enum FileTargetType {
     POST,              // 통합 게시글
     COUNCIL_POST,      // 자치회 활동 후기
     USER_PROFILE,
-    PINECONE_MEMORY
+    PINECONE_MEMORY,
+    MESSAGE            // 쪽지 첨부파일
 }

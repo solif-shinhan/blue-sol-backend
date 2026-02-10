1c3f2def42dc9d39b44f8c05479a4310378b3b2e
dhun <arrrrr180@gmail.com>
2026-02-10 11:20:05 +0900

    docs: FileTargetType 정리

--- blue-sol-backend/src/main/java/com/solif/backend/domain/file/entity/FileTargetType.java
+++ blue-sol-backend/src/main/java/com/solif/backend/domain/file/entity/FileTargetType.java
@@ -3,7 +3,6 @@ package com.solif.backend.domain.file.entity;
 public enum FileTargetType {
     POST,              // 통합 게시글
     COUNCIL_POST,      // 자치회 활동 후기
-    USER_PROFILE,
     PINECONE_MEMORY,
     MESSAGE,           // 쪽지 첨부파일
     MENTORING_CARD     // 멘토링 엽서 첨부파일
